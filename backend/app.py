79a867f0f491f11491217ac073a6ccaf826f653c
Tanzir Hoque <fan042002@gmail.com>
2024-01-21 03:51:49 -0500

    added a route

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/all/', methods=['GET'])
+def fetchData():
+    try:
+        return jsonify({'message': 'Test message'}), 200
+    except Exception as e:
+        print(e) #To print the type of error...
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,30 @@ import {
 
 
 function App() {
+  const promptBackEnd = async () => {
+    try {
+      const url = 'http://localhost:5100/all/';
+  
+      const response = await fetch(url, {
+        method: 'GET'
+      });
+  
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log(responseData);
+      } else {
+        console.log('Error:', response.status + '\n' + "Response: " + response.json());
+      }
+    } catch (error) {
+      console.log('Error:', response.status + '\n' + "Response: " + response.json());
+    }
+  };
+  
+  // Call the function to send data to Flask
+  promptBackEnd();
+  
+
+
   return (
     <div className="App">
       <header className="App-header">

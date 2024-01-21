ae2626163b10a81cdccda542302569426dde7b4a
Athmane Benarous <atho5850@gmail.com>
2024-01-21 08:45:25 -0500

    updated ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,36 @@
-import './App.css';
-import {
-  Stat,
-  StatLabel,
-  StatNumber,
-} from '@chakra-ui/react'
-import Graph from './components/graph'
-
+import { useState } from 'react';
+import { Flex, Box, Stat, StatLabel, StatNumber, Heading } from '@chakra-ui/react';
+import Graph from './components/graph';
 
 function App() {
+  const [revenue, setRevenue] = useState(0);
+
+  const updateRevenue = (newValue) => {
+    setRevenue(newValue);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>AutoScheduler</h1>
-        <h2></h2>
-        <Graph></Graph>
-        <Stat>
-        <StatLabel>Revenue of the Day</StatLabel>
-        <StatNumber>£0.00</StatNumber>
-        </Stat>
-      </header>
+      <Flex
+        direction="column"
+        align="center"
+        justify="center"
+        className="App-header"
+      >
+        <Heading as="h1" size="xl" textAlign="center" my="4">
+          AutoScheduler
+        </Heading>
+        <Graph updateRevenue={updateRevenue} />
+        <Flex direction="row" justify="center" align="center" mt="4">
+          <Stat>
+            <StatLabel>Total Revenue</StatLabel>
+            <StatNumber>${revenue}.00</StatNumber>
+          </Stat>
+          <Stat ml={3}>
+            <StatLabel>Revenue Loss</StatLabel>
+            <StatNumber>$0.00</StatNumber>
+          </Stat>
+        </Flex>
+      </Flex>
     </div>
   );
 }
--- frontend/src/components/graph.js
+++ frontend/src/components/graph.js
@@ -2,72 +2,67 @@ import React from 'react'
 import { useState } from 'react';
 import { Brush, Line, LineChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Area, AreaChart, ResponsiveContainer } from 'recharts'
 
-const data = [
-    {
-      "name": "",
-      "Compact": 4000,
-      "Medium": 2400,
-      "amt": 2400
-    },
-    {
-      "name": "01-02",
-      "Compact": 3000,
-      "Medium": 1398,
-      "amt": 2210
-    },
-    {
-      "name": "Page C",
-      "Compact": 2000,
-      "Medium": 9800,
-      "amt": 2290
-    },
-    {
-      "name": "Page D",
-      "Compact": 2780,
-      "Medium": 3908,
-      "amt": 2000
-    },
-    {
-      "name": "Page E",
-      "Compact": 1890,
-      "Medium": 4800,
-      "amt": 2181
-    },
-    {
-      "name": "Page F",
-      "Compact": 2390,
-      "Medium": 3800,
-      "amt": 2500
-    },
-    {
-      "name": "Page G",
-      "Compact": 3490,
-      "Medium": 4300,
-      "amt": 2100
-    }
-  ]
-  
-function Graph() {
-    return (
-        <LineChart width={750} height={250} data={data}
-    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-    <CartesianGrid strokeDasharray="3 3" />
-    <XAxis dataKey="name" />
-    <YAxis />
-     <Tooltip />
-     <Legend verticalAlign="top" height={36}/>
-    <Line type="monotone" dataKey="Compact" stroke="#8884d8" />
-    <Line type="monotone" dataKey="Medium" stroke="#82ca9d" />
-    <Brush dataKey="x" height={36} stroke="#8884d8" fill="transparent" >
-    <LineChart data={data}>
-    <CartesianGrid strokeDasharray="3 3" />
-    <XAxis dataKey="name" hide />
-    <YAxis hide />
-    <Line type="monotone" dataKey="Compact" stroke="#8884d8" />
-    <Line type="monotone" dataKey="Medium" stroke="#82ca9d" />
-        </LineChart>
-    </Brush>
-    </LineChart>
-    )
+
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const data = [];
+for (let i = 1; i <= 61; i++) {
+
+  data.push({
+    "name": "Day " + i.toString(),
+    "Compact": generateRandomNumber(1, 5),
+    "Medium": generateRandomNumber(1, 5),
+    "Fullsize": generateRandomNumber(1, 5),
+    "C1 Truck": generateRandomNumber(1, 5),
+    "C2 Truck": generateRandomNumber(1, 5),
+  });
+}
+
+
+function Graph({ updateRevenue }) {
+  const [brushIndex, setBrushIndex] = useState([0, 60])
+
+  // Function to update revenue when brush is changed
+  const handleBrushChange = (domain) => {
+    console.log(domain);
+    setBrushIndex([domain.startIndex, domain.endIndex]);
+    updateRevenue(data.slice(domain.startIndex, domain.endIndex).reduce((accumulator, current) => {
+      return accumulator + ((current.Compact * 150) + (current.Medium * 150) + (current.Fullsize * 150) + (current["C1 Truck"] * 250) + (current["C2 Truck"] * 700));
+    }, 0))
+
+  };
+
+  return (
+    <div>
+      <p>Brush Index: {brushIndex[0]} - {brushIndex[1]}</p>
+      <LineChart width={875} height={350} data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend verticalAlign="top" height={36} />
+        <Line type="monotone" dataKey="Compact" stroke="#8884d8" />
+        <Line type="monotone" dataKey="Medium" stroke="#82ca9d" />
+        <Line type="monotone" dataKey="Fullsize" stroke="#C70039" />
+        <Line type="monotone" dataKey="C1 Truck" stroke="#C700BE" />
+        <Line type="monotone" dataKey="C2 Truck" stroke="#FFCB08" />
+        <Brush dataKey="x" height={36} stroke="#000000" fill="transparent" onChange={handleBrushChange}>
+          <LineChart data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" hide />
+            <YAxis hide />
+            <Line type="monotone" dataKey="Compact" stroke="#8884d8" />
+            <Line type="monotone" dataKey="Medium" stroke="#82ca9d" />
+            <Line type="monotone" dataKey="Fullsize" stroke="#C70039" />
+            <Line type="monotone" dataKey="C1 Truck" stroke="#C700BE" />
+            <Line type="monotone" dataKey="C2 Truck" stroke="#FFCB08" />
+          </LineChart>
+        </Brush>
+      </LineChart>
+    </div>
+  )
 }
 export default Graph
